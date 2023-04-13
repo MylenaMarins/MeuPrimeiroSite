167a55819d022ff943bb73bce6d0773c52b749e7
Mylena Marins <129413069+MylenaMarins@users.noreply.github.com>
2023-04-13 10:24:52 -0300

    javascript separado

---
+++ JS/JavaScript.js
@@ -0,0 +1,7 @@
+ // Js para o meu primeiro site
+    i=prompt("Quantos anos você tem?")
+    if (i>=18) {
+      window.alert("Seja bem vindo(a)!");
+    } else {
+      window.alert("Chame um responsável para efetuar compras no site!");
+    }
